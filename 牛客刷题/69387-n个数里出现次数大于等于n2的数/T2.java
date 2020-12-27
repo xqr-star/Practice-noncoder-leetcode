cdf3ebb168b91276cbddb1e573b8d34d4bcdb0e7
xqr <1159641785@qq.com>
2020-12-27 20:12:03 +0800

    使用map 数据结构进行解题

---
+++ 牛客刷题/69387-n个数里出现次数大于等于n2的数/T2.java
@@ -0,0 +1,57 @@
+package code;
+import java.util.*;
+public class T2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String str = in.nextLine();
+            String[] strs = str.split(" ");//以，分割
+            int[] arr = new int[strs.length];
+
+            //字符串数组转换成int数组
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.valueOf(strs[i]);
+            }
+            //如果用数据结构来写这道题 -- map
+            //key 是数组中的每一个元素  value 是出现的次数
+            Map<Integer,Integer> map = new TreeMap<>();
+            for(int i =0 ;i < arr.length;i++){
+                if(map.containsKey(arr[i])){
+                    map.put(arr[i],map.get(arr[i])+1); // map.get(i) 返回key 对于的value
+                }else {
+                    map.put(arr[i],1);//Map中键值对的Key不能直接修改，value可以修改，如果要修改key，只能先将该key删除掉，然后再来进行重新插入
+                }                // set 中只有key 没有value 所以如果要修改key 必须先删除再修改
+            }
+
+            for(Map.Entry<Integer,Integer> entry : map.entrySet()){  //Set<Map.Entry<K, V>> entrySet() 返回所有的 key-value 映射关系
+                if(entry.getValue() >= arr.length/2){
+                    System.out.println(entry.getKey());
+                }
+            }
+
+
+        }
+    }
+}
+
+
+
+
+ /*     int count = 0;
+            int num = arr[0]; // 从第一个出现的位置开始计数
+            //其中有一个整数出现次数大于等于n/2。
+            Arrays.sort(arr);*/
+  /* //3 9 3 2 5 6 7 3 2 3 3 3
+            for (int j = 0; j < arr.length; j++) {
+                if(num == arr[j]){
+                    count++;
+                } else if (count > 0) {
+                count--;
+                } else {
+                    num = arr[j];
+                }
+            }
+            System.out.println(num);*/
+
+
