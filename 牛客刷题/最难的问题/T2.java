d545c564351fb259de114b65fe53995cf698867b
xqr <1159641785@qq.com>
2021-01-20 12:13:41 +0800

    通过此题练习一些API

--- 牛客刷题/最难的问题/T2.java
+++ 牛客刷题/最难的问题/T2.java
@@ -6,20 +6,50 @@ public class T2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()){
-            String [] arr = sc.nextLine().split(" ");
-            for(int i = 0 ;i < arr.length;i++){
+            String [] arr = sc.nextLine().split(" "); // 按照空格分放到数组里面
+            for(int i = 0 ;i < arr.length;i++){ //
                 //拿到数据进行解析
                 String str= transfer(arr[i]);
                 System.out.print(i != arr.length-1? str+" ": str);
             }
             System.out.println();
         }
-
-
-
     }
 
+
+
+
     private static String transfer(String s) {
+        //如果不转换成字符数组怎么做？
+        StringBuilder sb  = new StringBuilder();
+        for(int i =0 ;i < s.length();i++){
+            char ch = s.charAt(i);
+            if (ch > 'E') {
+                sb.append( (char)(ch-5));
+            }else {
+                sb.append( (char) (ch+21));
+            }
+        }
+       return sb.toString();
+    }
+
+    private static String transfer2(String s) {
+        char [] arr = s.toCharArray();
+        //拿到里面的一个个的字符
+        for(int i = 0; i<arr.length;i++){
+            if( arr[i] >= 'A' && arr[i] <= 'E') {
+                arr[i] = (char) (arr[i] +21);
+            }else {
+                arr[i] =(char) (arr[i]- 5);
+            }
+        }
+        String string = new String(arr);
+        return string;
+    }
+
+
+
+    private static String transfer1(String s) {
 
         char[] arr = s.toCharArray();
         for(int i = 0 ;i < arr.length;i++){
@@ -33,7 +63,7 @@ public class T2 {
         }
 
         //返回字符串
-        String str = new String(arr);
+        String str = new String(arr);//可以用一个字符数组来new 一个String 然后形成一个str
         return str;
 
     }
