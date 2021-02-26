44259bb763b0e4d0403eb35ef72ba81473d4ba7b
xqr <1159641785@qq.com>
2021-02-26 17:43:05 +0800

    剑指 Offer 04. 二维数组中的查找

--- Leetcode/数组/findNumberIn2DArray.java
+++ Leetcode/数组/findNumberIn2DArray.java
@@ -3,6 +3,28 @@ package array;
 //剑指 Offer 04. 二维数组中的查找
 //主题240
 public class findNumberIn2DArray {
+    class Solution {
+        public boolean findNumberIn2DArray(int[][] matrix, int target) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return false;
+            }
+            int rows = matrix.length, columns = matrix[0].length;
+            int row = 0, column = columns - 1;
+            while (row < rows && column >= 0) {
+                int num = matrix[row][column];
+                if (num == target) {
+                    return true;
+                } else if (num > target) {
+                    column--;
+                } else {
+                    row++;
+                }
+            }
+            return false;
+        }
+    }
+
+
     /**
      * 我竟然连暴力的都没有想到?!
      * @param matrix
