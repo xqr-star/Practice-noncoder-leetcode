8135b19806e883c4eabde702fe0021c4e544029d
xqr <1159641785@qq.com>
2021-03-03 22:54:46 +0800

    暴力法解决寻找第k大的元素

--- Leetcode/数组/findKthLargest.java
+++ Leetcode/数组/findKthLargest.java
@@ -1,84 +1,52 @@
 package array;
 
-import java.util.Random;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 //215. 数组中的第K个最大元素
 public class findKthLargest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,2,3,1,2,4,5,5,6};
+        findKthLargest2(arr,4);
+    }
+
     /**
-     * 堆排序
+     * 使用jdk自带的排序来做
+     * 然后遍历数组拿到倒数第k个
+     * 数组的排序中小规模的排序使用的是插入排序
+     * 大规模的是使用快排
+     * @param nums
+     * @param k
+     * @return
      */
-    class Solution {
-        public int findKthLargest(int[] nums, int k) {
-            int heapSize = nums.length;
-            buildMaxHeap(nums, heapSize);
-            for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
-                swap(nums, 0, i);
-                --heapSize;
-                maxHeapify(nums, 0, heapSize);
-            }
-            return nums[0];
-        }
+    public  static int findKthLargest2(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
 
-        public void buildMaxHeap(int[] a, int heapSize) {
-            for (int i = heapSize / 2; i >= 0; --i) {
-                maxHeapify(a, i, heapSize);
-            }
-        }
-
-        public void maxHeapify(int[] a, int i, int heapSize) {
-            int l = i * 2 + 1, r = i * 2 + 2, largest = i;
-            if (l < heapSize && a[l] > a[largest]) {
-                largest = l;
-            }
-            if (r < heapSize && a[r] > a[largest]) {
-                largest = r;
-            }
-            if (largest != i) {
-                swap(a, i, largest);
-                maxHeapify(a, largest, heapSize);
-            }
-        }
-
-        public void swap(int[] a, int i, int j) {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
     }
-
-
-
-    Random random = new Random();
-    public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
-    }
-    public int quickSelect(int[] a, int l, int r, int index) {
-        int q = randomPartition(a, l, r);
-        if (q == index) {
-            return a[q];
-        } else {
-            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+    /**
+     * JDK的优先级队列中使用的是小堆
+     * 如果要找的是第k个最大的，那么实际上就可以
+     * 倒着去寻找第k个
+     * 由于k总是有效的，所以不需要多考虑什么
+     * 数组的元素个数是arr.length 要获取的是倒数第k个
+     * 前面需要删除的是arr.length- k 然后再获取堆顶的元素
+     * 堆并不能做到有序性，只能做上一层和这一层的关系，无法保证一层里面的关系
+     * 所以如果使用建立小堆的方式是做不到的
+     * 必须建立大堆，一旦建立大堆，就需要考虑到自己实现的问题
+     *
+     * 优先级队列，虽然看起来是二叉树的结构，但是本质上是一个数组的结构
+     *
+     * 错误解法，不应该采用
+     * */
+    public  static int findKthLargest1(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i = 0;i< nums.length;i++){
+            queue.add(nums[i]);
         }
-    }
-    public int randomPartition(int[] a, int l, int r) {
-        int i = random.nextInt(r - l + 1) + l;
-        swap(a, i, r);
-        return partition(a, l, r);
-    }
-    public int partition(int[] a, int l, int r) {
-        int x = a[r], i = l - 1;
-        for (int j = l; j < r; ++j) {
-            if (a[j] <= x) {
-                swap(a, ++i, j);
-            }
+        for(int i = 0;i <= nums.length -k;i++){
+            queue.remove();
         }
-        swap(a, i + 1, r);
-        return i + 1;
+        return queue.remove();
     }
-    public void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-
 }
