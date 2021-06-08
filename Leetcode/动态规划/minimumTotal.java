24193b80944d3016a1cb90939c8c7d57b61f7537
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-06-08 22:58:41 +0800

    120. 三角形最小路径和

--- Leetcode/动态规划/minimumTotal.java
+++ Leetcode/动态规划/minimumTotal.java
@@ -1,40 +1,121 @@
-package Leetcode;
+package lc2;
 
+import java.util.LinkedList;
 import java.util.List;
 
 //120. 三角形最小路径和
 public class minimumTotal {
+
+    /**
+     * 滚动数组修改状态  由于当前的状态之和上一行的状态有关 所以只用一维数组就可以做到
+     * @param triangle
+     * @return
+     */
     public static void main(String[] args) {
+        List<List<Integer>> lists = new LinkedList<>();
 
     }
 
+
     public int minimumTotal(List<List<Integer>> triangle) {
-
-        int n = triangle.size();
-
-        //开辟数组的大小，无法做到动态的形式
-        int[][] dp = new int[n][n];
-
-        //初始化状态
+        int length = triangle.size();
+        int[][] dp = new int[2][length];
         dp[0][0] = triangle.get(0).get(0);
-
-        for(int i = 1 ;i < n;i++){
-            for(int j = 0; j <= i;j++){
-                dp[i][j] = Integer.MAX_VALUE;
-                if(j > 0) dp[i][j] = Math.min(dp[i-1][j-1],dp[i-1][j]) + triangle.get(i).get(j);
-                //但是情况需要考虑 i-1 和 j-1 可能越界
-                if(j == 0) dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
-                if(j == i) dp[i][j] = dp[i-1][j-1]+triangle.get(i).get(j); //如果是最右边的，只能是从左边下来的
-                //这里是常规的状态转移方程
-
+        for(int i = 1; i < length;i++){
+            for(int j = 0;j < triangle.get(i).size();j++){
+                //处理边界情况
+                if(j == 0){
+                    dp[i&1][j] = dp[i-1 &1][j]+triangle.get(i).get(j);
+                }else  if (j == triangle.get(i).size()-1){
+                    dp[i&1][j] = dp[i-1 &1][j-1]+triangle.get(i).get(j);
+                }else {
+                    dp[i&1][j] = Math.min(dp[i - 1&1][j], dp[i - 1&1][j - 1]) + triangle.get(i).get(j);
+                }
             }
         }
-
-        //之后遍历一遍最后一行记录数据数组 进行更新最小值
-        int res  = Integer.MAX_VALUE;
-        for(int j = 0;j < n;j++){
-            res = Math.min(res,dp[n-1][j]);
+        int min = Integer.MAX_VALUE;
+        for(int m = 0; m < triangle.get(length-1).size();m++){
+            min = Math.min(min,dp[1][m]);
         }
-        return res;
+        return min;
+    }
+
+
+
+    /**
+     * 错误版本
+     * @param triangle
+     * @return
+     */
+    public  static int minimumTotal3(List<List<Integer>> triangle) {
+        int length = triangle.size();
+        int[] dp  = new int[length];
+        dp[0] = triangle.get(0).get(0);
+        for(int i = 1; i < length;i++){
+            for(int j = 0;j <= i;j++){
+                if(j ==0) {
+                    dp[j] =dp[j]+triangle.get(i).get(j);
+                }else if(j == i){
+                    dp[j] =dp[j-1]+triangle.get(i).get(j);
+                }else {
+                    dp[j] = Math.min(dp[j],dp[j-1])+triangle.get(i).get(j);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int m = 0; m < length;m++){
+            min = Math.min(min,dp[m]);
+        }
+        return min;
+    }
+
+
+
+    //需要创建一个二维数组，多开辟一些空间 来记录
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        int length = triangle.size();
+        int[][] dp = new int[length][length];
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i = 1; i < length;i++){
+            for(int j = 0;j < triangle.get(i).size();j++){
+                dp[i][j] = Integer.MAX_VALUE;
+                //处理边界情况
+                if(j == 0){
+                    dp[i][j] = dp[i-1][j]+triangle.get(i).get(j);
+                }else  if (j == triangle.get(i).size()-1){
+                    dp[i][j] = dp[i-1][j-1]+triangle.get(i).get(j);
+                }else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle.get(i).get(j);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int m = 0; m < triangle.get(length-1).size();m++){
+            min = Math.min(min,dp[length-1][m]);
+        }
+        return min;
+    }
+
+
+    //要考虑每一层只可以移动的相邻的结点这个问题
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        int length = triangle.size();
+        //用来记录 第i层的最小值
+        int[] f = new int[length];
+        f[0] = triangle.get(0).get(0);
+        //第一个值
+        int num = Integer.MIN_VALUE;
+
+        //依次遍历每一层
+        for (int i = 1 ;i < length;i++){
+            //依次得到这一层的每一个
+            int min = triangle.get(i).get(0);
+            for(int j = 0; j < triangle.get(i).size();j++){
+                min = Math.min(min,triangle.get(i).get(j));
+            }
+            f[i] = f[i-1]+min;
+        }
+        return f[length-1];
+
     }
 }
