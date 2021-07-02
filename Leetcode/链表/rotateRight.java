fd0c86058156316d86bfc3f7d0ef143c68fc0c06
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-07-02 23:45:12 +0800

    61. 旋转链表

--- Leetcode/链表/rotateRight.java
+++ Leetcode/链表/rotateRight.java
@@ -1,86 +1,35 @@
-package ListNode;
-
 //61. 旋转链表
 public class rotateRight {
-    /**
-     * 方法二 ： 采用快慢指针的做法
-     * @param head
-     * @param k
-     * @return
-     */
-    public ListNode rotateRight2(ListNode head, int k) {
-        if(head == null|| k == 0) return head;
-        ListNode fakeHead = new ListNode(-1);
-        fakeHead.next = head;
-        //第一步求出链表的长度
-        int length = 0;
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode fake = new ListNode(-1);
+        fake.next  = head;
+        //首先对k 和 head 进行校验
+        if(head == null) return null;
+        //求链表的长度 然后取模 缩小k的范围
         ListNode cur = head;
+        int count = 0;
         while(cur != null){
-            length++;
+            count++;
             cur = cur.next;
         }
-        //第二步判断对应的index
-        int key = k % length;
-        if(key == 0) return fakeHead.next;
-        //采用快慢指针的做法
-        //让快指针先走k步 然后和慢指针 一个一步的走 直到快指针走到最后一个
-        ListNode fast = head;
-        while (key >0){
+        //注意k的长度判定
+        k = k %count;
+        if(k == 0) return head;
+
+        //也就是找到倒数的第k个的前一个
+        ListNode fast = fake;
+        ListNode slow = fake;
+        while( k > 0){
             fast = fast.next;
-            key--;
+            k--;
         }
-        ListNode low = head;
-        while (fast != null && fast.next != null){
+        while(fast.next != null){
+            slow = slow.next;
             fast = fast.next;
-            low = low.next;
         }
-        //快指针指向的是最后一个
-        //慢指针和它的的下一个都是需要更改指向的
-        fakeHead.next = low.next;
-        low.next = null;
-        fast.next = head;
-        return fakeHead.next;
-    }
-
-    /**
-     * 普通的思想 就是找到对应的位置 然后只用进行下标的更改就可以了
-     * 不用一个一个的链表进行翻转
-     * @param head
-     * @param k
-     * @return
-     */
-
-    public ListNode rotateRight1(ListNode head, int k) {
-        if(head == null|| k == 0) return head;
-        ListNode fakeHead = new ListNode(-1);
-        fakeHead.next = head;
-        //第一步求出链表的长度
-        int length = 0;
-        ListNode cur = head;
-        while(cur != null){
-            length++;
-            cur = cur.next;
-        }
-        //第二步判断对应的index
-        int key = k % length;
-        if(key == 0) return fakeHead.next;
-
-        //需要移动的次数
-        int move = (length - key);
-        //第三步走到需要移动的第一个结点
-        cur = head;
-        ListNode pre = fakeHead;
-        while(move >0){
-            pre = cur;
-            cur = cur.next;
-            move--;
-        }
-        pre.next = null;
-        fakeHead.next = cur;
-        while(cur.next != null){
-            cur = cur.next;
-        }
-        cur.next = head;
-        return fakeHead.next;
+        fast.next = fake.next;
+        fake.next = slow.next;
+        slow.next = null;
+        return fake.next;
     }
 }
