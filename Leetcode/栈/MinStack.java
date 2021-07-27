2d5f643d672f559feeae979318b4c5c057991491
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-07-27 23:54:23 +0800

    剑指 Offer 30. 包含min函数的栈

--- Leetcode/栈/MinStack.java
+++ Leetcode/栈/MinStack.java
@@ -1,99 +1,52 @@
-<<<<<<< HEAD
-package Stack;
-
 import java.util.Deque;
 import java.util.LinkedList;
 
-//155. 最小栈
-//可以迅速返回栈里面元素--时间的复杂度是o(1)
-
-/**
- * 准备两个栈，一个栈做就是正常的元素进栈出栈
- * 另一个栈永远保存的是当前的情况下最小的元素
- * 保持两个栈具有相同的size,
- * 同进 一个进正常值 一个进当前的最小值
- * 同出
- */
+//剑指 Offer 30. 包含min函数的栈
 public class MinStack {
+
+    /**
+     * 残差法
+     */
+    Deque<Long> stack;
+    long min;
     /** initialize your data structure here. */
-    private Deque<Integer> stack;
-    private Deque<Integer> minStack;
     public MinStack() {
-       stack = new LinkedList<>();
-       minStack = new LinkedList<>();
+        stack = new LinkedList<>();
     }
 
-    public void push(int x) {
-        stack.push(x);
-        if(minStack.isEmpty() || x < minStack.peek() ){
-            minStack.push(x);
-        }else {
-            minStack.push(minStack.peek());//相当于入栈的时候，最小栈里面入栈的是当前最小的一个元素
+    public void push(long x) {
+        if(stack.isEmpty()){
+            min = x;
+            stack.push(0l);
+        }else{
+            stack.push(x - min);
+            // 小于min则更新
+            min = x < min? x: min;
         }
-
     }
 
-    public void pop() { // 出栈的时候要保证两个同时出栈
-        stack.pop();
-        minStack.pop();
-    }
-
-    public int top() {
-        return stack.peek();
-
-    }
-
-    public int getMin() {
-        return minStack.peek();
-    }
-}
-=======
-package Stack;
-
-import java.util.Deque;
-import java.util.LinkedList;
-
-//155. 最小栈
-//可以迅速返回栈里面元素--时间的复杂度是o(1)
-
-/**
- * 准备两个栈，一个栈做就是正常的元素进栈出栈
- * 另一个栈永远保存的是当前的情况下最小的元素
- * 保持两个栈具有相同的size,
- * 同进 一个进正常值 一个进当前的最小值
- * 同出
- */
-public class MinStack {
-    /** initialize your data structure here. */
-    private Deque<Integer> stack;
-    private Deque<Integer> minStack;
-    public MinStack() {
-       stack = new LinkedList<>();
-       minStack = new LinkedList<>();
-    }
-
-    public void push(int x) {
-        stack.push(x);
-        if(minStack.isEmpty() || x < minStack.peek() ){
-            minStack.push(x);
+    public void pop() {
+        //看是否需要更新min
+        if(stack.peek() < 0){
+            //说明需要更新一下最小值
+            min = min - stack.pop();
         }else {
-            minStack.push(minStack.peek());//相当于入栈的时候，最小栈里面入栈的是当前最小的一个元素
+            stack.pop();
         }
-
     }
 
-    public void pop() { // 出栈的时候要保证两个同时出栈
-        stack.pop();
-        minStack.pop();
+    public long top() {
+        //如果当前的栈顶小于0 那么top 就是min
+        if(stack.peek() < 0) {
+            return  min;
+        }else {
+            return  stack.peek() + min;
+        }
     }
 
-    public int top() {
-        return stack.peek();
-
+    public long min() {
+        return min;
     }
 
-    public int getMin() {
-        return minStack.peek();
-    }
 }
->>>>>>> 5c2b30d43db8758c498135a65f852414a31c98c2
+
