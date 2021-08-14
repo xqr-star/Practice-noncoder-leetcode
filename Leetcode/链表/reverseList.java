9f88b11ac4fac6bb69aeb3c6e3f982cb91331b31
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-08-14 23:49:23 +0800

    剑指 Offer 24. 反转链表

--- Leetcode/链表/reverseList.java
+++ Leetcode/链表/reverseList.java
@@ -1,23 +1,18 @@
-package Node;
-//24. 反转链表
+//剑指 Offer 24. 反转链表
 public class reverseList {
-    /**
-     * 反转链表
-     * @param head
-     * @return
-     */
-    public ListNode solution(ListNode head) {
-        //为了防止错过头结点，那么就先定义一个空结点
-        ListNode prev = null;
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = new ListNode(-1);
+        prev.next = head;
         ListNode cur = head;
-        ListNode curNext = head;
+        if(head == null || head.next == null) return head;
+        ListNode curNext = null;
         while(cur != null){
             curNext = cur.next;
             cur.next = prev;
             prev = cur;
             cur = curNext;
         }
+        head.next = null;
         return prev;
     }
 }
-
