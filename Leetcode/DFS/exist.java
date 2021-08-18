37145f1c20cacc47fd11a20440779ccc6b89ebe4
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-08-18 23:33:32 +0800

    剑指 Offer 12. 矩阵中的路径

--- Leetcode/DFS/exist.java
+++ Leetcode/DFS/exist.java
@@ -1,55 +1,53 @@
-package DFS;
-
 //剑指 Offer 12. 矩阵中的路径
 public class exist {
-    /**
-     * 深度优先搜索 ： 先暴力遍历矩阵中的所有字符串
-     * dfs 递归，先朝一个方向搜索到底，然后回溯至上一个结点 沿另一个方向搜索 依次类推
-     *
-     * 剪枝 ： 在搜索的过程中 如果遇到了和目标字符不匹配的情况（也就是dfs退出条件），立即返回
-     * @param board
-     * @param word
-     * @return
-     */
+    static String  str ;
 
-    public boolean exist(char[][] board, String word) {
-        //将字符串转换为字符数组
-        char[] arr = word.toCharArray();
+    public static void main(String[] args) {
+        char[][] board = new char[][] {{'a','b','c','d','e'},{'s','f','c','s'},{'a','d','e','f'}};
+        String str = "abcced";
+        System.out.println(exist(board, str));
+    }
+    public  static boolean exist(char[][] board, String word) {
+        if(board == null) return false;
+        str = word;
+        int index = 0; //表示需要匹配的第一个字符
 
-        //然后遍历board当中的所有元素 对每一个元素都进行深度优先遍历
-        int level = board.length;
-        int column = board[0].length;
-        for(int i = 0;i < level;i++) {
-            for (int j = 0; j < column; j++) {
-                boolean flag = dfs(board, arr, i, j, 0);
-                //这里相当于就是一个剪枝的过程
-                if (flag) return true;
+        for(int i = 0; i< board.length;i++){
+            for(int j = 0; j< board[0].length;j++){
+                boolean res = dfs(board,0,i,j);
+                if (res) return true;
             }
         }
-        return  false;
+        return false;
     }
+    public  static boolean dfs(char[][] board,int index,int i,int j){
 
-    private boolean dfs(char[][] board, char[] arr, int i, int j, int k) {
-        //不断递归的条件
+        if(index == str.length()) return true; //所有都以及找到了
 
-        //停止的条件 --
-        // 索引越界  / 字符串不匹配
-        if(i < 0 || i >= board.length ||j < 0 || j >= board[0].length||board[i][j] != arr[k]) return false;
+        if(i<0 || i >= board.length || j <0 || j >= board[0].length){
+            //越界了
+            return false;
+        }
+        char ch = board[i][j];
+        if(board[i][j] == '.' || board[i][j] != str.charAt(index)) return false;
 
-        //递归停止的条件 最后一个字符串都已经匹配了，说明可以返回结果了 不需要继续递归了否则会越界
-        if(k == arr.length - 1) return true;
-        //说明第一个字符已经可以正确的进行匹配 对这个字符进行标记 防止之后 其他字符搜索到这个字符
-        //为什么要标记成 \0呢？ -- 表示字符  ‘\0’是字符串来结束标志位  对应ascii 码：00；  ‘0’是字符0  对应ascii 码：30
-        board[i][j] = '\0';
+        //如果当前的这个位置没有走过  并且字符匹配 表示匹配
+        board[i][j] = '.';
+        //当前已经找到啦，继续去进行搜索上下左右
+        boolean res = dfs(board,index+1,i+1,j) || dfs(board,index+1,i-1,j) || dfs(board,index+1,i,j-1)
+                || dfs(board,index+1,i,j+1);
 
-        //然后对这字符的上下左右 进行深度优先搜索 看看是否和之后的字符相等
-        //关于到底上下左右谁写在前面的问题是 由于|| 运算符一个true 都是true 那么 谁写在前面 其实变相的决定了 运行时间
-        boolean res = dfs(board,arr,i-1,j,k+1) || dfs(board,arr,i+1,j,k+1) ||
-                dfs(board,arr,i,j+1,k+1) || dfs(board,arr,i ,j-1,k+1);
 
-        //每一个搜索的结果返回之后 都要把这个值重新放回去
-        board[i][j] = arr[k];
-
+        //进行回溯
+        board[i][j] = ch;
         return res;
+
+
+        // 虽然当前这个字符匹配，但是之后的不行 ,继续进行搜索上下左右
+        //这样写的话枚举的起点都变了
+//        return dfs(board,index,i+1,j) || dfs(board,index,i-1,j) || dfs(board,index,i,j-1)
+//                || dfs(board,index,i,j+1);
+
+
     }
 }
