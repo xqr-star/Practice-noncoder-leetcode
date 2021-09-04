424853f5a7552411623d65485940e2cf83c62ac5
xqr-star <65577085+xqr-star@users.noreply.github.com>
2021-09-04 22:16:43 +0800

    128. 最长连续序列

--- Leetcode/字符/longestConsecutive.java
+++ Leetcode/字符/longestConsecutive.java
@@ -70,31 +70,71 @@ public class longestConsecutive {
     }
 
     //最长递增字符串 输入abcabcdefad  输出：abcdef
+    public static void main1(String[] args) {
+        String  str = "cdabijklmnabihdc";
+//        if(str == null) return null;
+//        if(str.length() == 0) return "";
 
-    public static void main(String[] args) {
-        String str = "abcdefg";
+        str = str.trim();
         char[] arr = str.toCharArray();
-        int max = 1;
+
         int count = 1;
+        int max = 1;
         int end = 0;
 
         for(int i = 1;i < arr.length;i++){
             if(arr[i] - arr[i-1] == 1){
-                end = i+1;
                 count++;
+                end = i+1;
                 max = Math.max(max,count);
-            }else {
+            }else{
                 count = 1;
             }
         }
-        //注意完全递增的情况也需要更新
+        //处理连续最长的
+        max = Math.max(max,count);
+        String res ;
         if(end == arr.length){
-            max = Math.max(max,count);
-            System.out.println(str.substring(end - max));
-        }else {
-            System.out.println(str.substring(end - max, end + 1));
+            res = str.substring(end-max,end);
+        }else{
+            res = str.substring(end-max,end+1);
         }
+        System.out.println(res);
+    }
 
-        System.out.println(max);
+
+    public static String findStr(String str){
+        if(str == null) return null;
+        if(str.length() == 0) return "";
+
+        str = str.trim();
+        char[] arr = str.toCharArray();
+
+        int count = 1;
+        int max = 1;
+        int end = 0;
+
+        for(int i = 1;i < arr.length;i++){
+            if(arr[i] - arr[i-1] == 1){
+                count++;
+                if(count > max){
+                    max = count;
+                    end = i;
+                }
+
+            }else{
+                count = 1;
+            }
+        }
+        //处理连续最长的
+        max = Math.max(max,count);
+        String res ;
+        if(end == arr.length){
+            res = str.substring(end-max,end);
+        }else{
+            res = str.substring(end-max+1,end+1);
+        }
+        System.out.println(res);
+        return res;
     }
 }
